b806caf54e5a4e59b6a318db65359f157b8974aa
mdekabs <1mawoda@gmail.com>
2023-05-26 01:37:07 +0100

    completed the basemodel

--- Backend/models/basemodel.py
+++ Backend/models/basemodel.py
@@ -20,4 +20,48 @@ import uuid
 
 
 class BaseModel:
-    pass
+    """ All other classes will inherit from this class."""
+    def __init__(self, *args, **kwargs):
+        """Initialization"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+        #loop through the keyword args and set the attributes
+        for key, value in kwargs.items():
+            if key == 'type()':
+                continue
+            setattr(self, key, value)
+            if type(self.created_at) is str: #check if its a string data type
+                self.created = datetime.fromisoformat(value)
+            if type(self.updated_at) is str:
+                self.updated_at = self.created_at
+    
+    def __str__(self):
+        """How BaseModel should be represented in string"""
+        return f("[{type(self).__name__}] ({self.id}) {self.dict}")
+
+    def save(self):
+        """ a function that updates the attribute with the current datetime an save in db"""
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def delete(self):
+        """A function to delete object saved in storage"""
+        models.storage.delete(self)
+
+    def to_dict(self):
+        """Returns a dictionary of the object class"""
+        #set and empty dictionary
+        New_dict = {}
+        New_dict["type()"] = type(self).__name__
+        #Loop throught the dict
+        for key, value in vars(self).items():
+            if key == "_sa_instance_state":
+                pass
+            elif isinstance(value, datetime.datetime):
+                New_dict[key] = value.isoformat()
+            else:
+                New_dict[key] = value
+        return New_dic
