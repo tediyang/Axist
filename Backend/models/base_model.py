2c6e386b6110e7515f7c1d6bb8291ef13a86b9fc
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-27 01:24:58 +0100

    updates user and base model

--- Backend/models/base_model.py
+++ Backend/models/base_model.py
@@ -19,7 +19,6 @@
 from datetime import datetime
 from models import storage
 from sqlalchemy import Column, String, DateTime
-#from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import declarative_base
 import uuid
 
--- Backend/models/user.py
+++ Backend/models/user.py
@@ -14,7 +14,6 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     username = Column(String(64), unique=True, nullable=False)
     password = Column(String(64), nullable=False)
-    password_reset_token = Column(String(64), nullable=False)
     email = Column(String(64), nullable=False)
     first_name = Column(String(64), nullable=True)
     last_name = Column(String(64), nullable=True)
@@ -28,16 +27,3 @@ class User(BaseModel, Base):
         if name == "password":
             value = md5(value.encode()).hexdigest()
         super().__setattr__(name, value)
-
-    def generate_password_reset_token(self):
-        """ we make a unique token that is sent to user email to reset password """
-        token = secrets.token_hex(16)
-        self.password_reset_token = token
-
-    def reset_password(self, new_password):
-        """ reset the password to the new one """
-        self.password = new_password
-        self.reset_token = None
-
-    def password_to_email(self):
-        pass
