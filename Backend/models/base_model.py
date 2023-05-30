0b23e11fba44cc58836412f15235112d55bedfff
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-30 16:15:21 +0100

    update on base_model and addition of initialization values of created_at and updated_at

--- Backend/models/base_model.py
+++ Backend/models/base_model.py
@@ -32,6 +32,7 @@ class BaseModel:
 
     # Class attributes which other models will inherit.
     id = Column(String(60), primary_key=True)
+    
 
     # Generate default values in the database.
     created_at = Column(DateTime, default=datetime.utcnow)
@@ -40,6 +41,8 @@ class BaseModel:
     def __init__(self):
         """ initialization of the id instance """
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """ string representation of the Object class """
@@ -54,11 +57,12 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-            new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         if "password" in new_dict:
