bdfdc30c1ebc475a888620cb5ffe2d8450b176d0
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-31 17:06:31 +0100

    pycodestyle

--- Backend/models/geolocation.py
+++ Backend/models/geolocation.py
@@ -9,13 +9,13 @@ from sqlalchemy import Column, String, ForeignKey
 
 
 class Location(BaseModel, Base):
-	""" Representation of a location data. """
-	__tablename__ = 'locations'
-	latitude = Column(String(64), nullable=False)
-	longitude = Column(String(64), nullable=False)
-	loc_name = Column(String(64), nullable=False)
-	user_id = Column(String(64), ForeignKey('users.id'), nullable=False)
+    """ Representation of a location data. """
+    __tablename__ = 'locations'
+    latitude = Column(String(64), nullable=False)
+    longitude = Column(String(64), nullable=False)
+    loc_name = Column(String(64), nullable=False)
+    user_id = Column(String(64), ForeignKey('users.id'), nullable=False)
 
-	def __init__(self):
-		""" Initializes location """
-		super().__init__()
+    def __init__(self):
+        """ Initializes location """
+        super().__init__()
