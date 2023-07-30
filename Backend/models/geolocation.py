aa30a4fb0483fae74595e0dd105d0dabc24b4fa0
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-07-30 02:27:50 +0100

    added *args and **kwargs to __init__

--- Backend/models/geolocation.py
+++ Backend/models/geolocation.py
@@ -6,16 +6,17 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+# from sqlalchemy.orm import relationship
 
 
 class Location(BaseModel, Base):
     """ Representation of a location data. """
     __tablename__ = 'locations'
+    user_id = Column(String(64), ForeignKey('users.id'), nullable=False)
     latitude = Column(String(64), nullable=False)
     longitude = Column(String(64), nullable=False)
-    loc_name = Column(String(64), nullable=False)
-    user_id = Column(String(64), ForeignKey('users.id'), nullable=False)
+    saved_name = Column(String(64), nullable=False)
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializes location """
-        super().__init__()
+        super().__init__(*args, **kwargs)
